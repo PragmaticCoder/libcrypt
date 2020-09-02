e88c9738175a63abbdc3703b35d42d7aec9f5330
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-09-01 22:32:31 -0600

    ✅ TEST: Beefed up our Library with few tests - wrote tests for _encode and _decode

--- tests/test_libcrypto.py
+++ tests/test_libcrypto.py
@@ -2,7 +2,18 @@
 
 """Tests for `src` package."""
 
-from src import libcrypt
+from src.libcrypt import LibCrypt
 
 
+def test_encode_success():
+    codes = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+    content = 'another message here!'
 
+    assert LibCrypt(codes=codes)._encode(content) == '1n4th2r m2ss1g2 h2r2!'
+
+
+def test_decode_success():
+    codes = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+    content = 'th3s 3s 1 m2ss1g2.'
+
+    assert LibCrypt(codes=codes)._decode(content) == 'this is a message.'

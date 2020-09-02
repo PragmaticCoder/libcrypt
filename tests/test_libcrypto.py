0b2aace490b13f33d0fe1419386c04568c0c4b05
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-09-01 22:34:09 -0600

    ✅ TEST: Similarly implemented test to check if transorm method is performing encription and decryption successfully

--- tests/test_libcrypto.py
+++ tests/test_libcrypto.py
@@ -17,3 +17,17 @@ def test_decode_success():
     content = 'th3s 3s 1 m2ss1g2.'
 
     assert LibCrypt(codes=codes)._decode(content) == 'this is a message.'
+
+
+def test_transform_returns_encrypted_string():
+    codes = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+    content = 'another message here!'
+
+    assert LibCrypt(codes=codes).transform(content) == '1n4th2r m2ss1g2 h2r2!'
+
+
+def test_transform_returns_decrypted_string():
+    codes = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+    content = 'th3s 3s 1 m2ss1g2.'
+
+    assert LibCrypt(codes=codes).transform(content) == 'this is a message.'

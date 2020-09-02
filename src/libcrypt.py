003d29a8a68dfc3b8457a3f8a5f7bd11349bf85d
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-09-01 22:11:23 -0600

    📦 NEW: Initial Planning - Added some TODO statements and implemented some skeleton methods and artifacts

--- src/libcrypt.py
+++ src/libcrypt.py
@@ -1,5 +1,47 @@
-"""Main module."""
-
-
 class LibCrypt(object):
-    pass
+    def __init__(self, codes):
+        # TODO:
+        # Initialize codes dict
+        # Ensure nums to string conversion here
+        pass
+
+    @property
+    def _decryption_codes(self):
+        # TODO:
+        # Reverse Key Value Pair
+        pass
+
+    @staticmethod
+    def _contains_digit(content):
+        # TODO:
+        # Check if string contains any digits
+        pass
+
+    def _encode(self, content):
+        # TODO:
+        # Sting to Number mapping
+        pass
+
+    def _decode(self, content):
+        # TODO:
+        # Number to String Mapping
+        pass
+
+    def transform(self, content):
+        # TODO:
+        # Invoke correct method based of if there exists any digits in given string
+        pass
+
+
+def main():
+    codes = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+
+    content = 'th3s 3s 1 m2ss1g2.'
+    print(LibCrypt(codes=codes).transform(content))
+
+    content = 'another message here!'
+    print(LibCrypt(codes=codes).transform(content))
+
+
+if __name__ == '__main__':
+    main()
